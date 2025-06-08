9ac72a7fdcaead303b7c3093d85780a2c72e0b1b
Rabey <avrabey@gmail.com>
2025-06-08 21:53:57 +0530

    V3- implemented Home page

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -2,7 +2,7 @@ class FixturesController < ApplicationController
   def today
     today = Date.today.to_s
 
-    url = "https://v3.football.api-sports.io/fixtures?date=#{today}"
+    url = "https://v3.football.api-sports.io/fixtures?date=#{today}&timezone=Asia/Kolkata"
 
   response = HTTParty.get(url, headers: {
     "x-apisports-key" => ENV["API_FOOTBALL_KEY"]
@@ -10,5 +10,17 @@ class FixturesController < ApplicationController
 
 
         render json: response.parsed_response
+  end
+
+  def show
+    fixture_id = params[:id]
+
+    url = "https://v3.football.api-sports.io/fixtures?id=#{fixture_id}&timezone=Asia/Kolkata"
+
+    response =  HTTParty.get(url, headers: {
+          "x-apisports-key" => ENV["API_FOOTBALL_KEY"]
+    })
+
+    render json: response.parsed_response
   end
 end
