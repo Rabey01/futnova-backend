d5d19456792d0427ddb918b7fb3e1e5f8e44cafd
Rabey <avrabey@gmail.com>
2025-06-15 16:52:19 +0530

    Linked league page

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -23,4 +23,52 @@ class FixturesController < ApplicationController
 
     render json: response.parsed_response
   end
+
+  LEAGUE_CODE_MAP = {
+    140 => "PD",   # La Liga
+    39  => "PL",   # Premier League
+    135 => "SA",   # Serie A
+    2   => "CL",   # UCL
+    1   => "WC",   # World Cup
+    61  => "FL1",  # Ligue 1
+    78  => "BL1"   # Bundesliga
+  }
+  def standings
+    code = LEAGUE_CODE_MAP[params[:id].to_i]
+    return render json: { error: "Unsupported league" }, status: :not_found unless code
+
+    url = "https://api.football-data.org/v4/competitions/#{code}/standings"
+    response = HTTParty.get(url, headers: {
+       "X-Auth-Token" => ENV["FOOTBALL_DATA_KEY"]
+    })
+
+    render json: response.parsed_response
+  end
+
+  def league_fixtures
+    code = LEAGUE_CODE_MAP[params[:id].to_i]
+    return render json: { error: "Unsupported league" }, status: :not_found unless code
+
+    comp_res = HTTParty.get("https://api.football-data.org/v4/competitions/#{code}", headers: {
+      "X-Auth-Token" => ENV["FOOTBALL_DATA_KEY"]
+    })
+
+    start_date = comp_res.parsed_response.dig("currentSeason", "startDate")
+    year = Date.parse(start_date).year rescue nil
+    return render json: { error: "Season info not found" }, status: :bad_gateway unless year
+
+    fixtures_url = "https://api.football-data.org/v4/competitions/#{code}/matches?season=#{year}"
+    response = HTTParty.get(fixtures_url, headers: {
+    "X-Auth-Token" => ENV["FOOTBALL_DATA_KEY"]
+  })
+
+  puts "Fetching fixtures..."
+  puts "League ID: #{params[:id]}"
+  puts "Mapped Code: #{code}"
+  puts "URL: #{fixtures_url}"
+  puts "Response Code: #{response.code}"
+  puts "Response Body: #{response.body}"
+
+    render json: response.parsed_response
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "/fixtures/today", to: "fixtures#today"
   get "/fixtures/:id", to: "fixtures#show"
+  get "/leagues/:id/standings", to: "fixtures#standings"
+  get "/leagues/:id/fixtures", to: "fixtures#league_fixtures"
 
   # Defines the root path route ("/")
   # root "posts#index"
